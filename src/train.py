c2abc6696d78b0ba94b5304e243f896bce9fd019
Simon Ortiz <simonortiz95@gmail.com>
2026-02-21 17:11:28 -0300

    Documenta train.py

--- src/train.py
+++ src/train.py
@@ -1,3 +1,11 @@
+'''
+Este script se encarga del entrenamiento del modelo de detección basado en YOLOv8, 
+utilizando un dataset personalizado definido en data.yaml.
+
+Este archivo define constantes para asegurar consistencia y coherencia.
+
+Se utiliza una función que encapsula la lógica.
+'''
 from ultralytics import YOLO
 
 MODELO = "yolov8s.pt"
@@ -9,6 +17,12 @@ NOMBRE_MODELO = "detector"
 
 def entrenar_modelo():
 
+    '''
+    Esta función tiene el objetivo de entrenar el modelo YOLO preentrenado 
+    en la versión elegida, donde se establecen distintos parámetros como las epocas,
+    y tamaño de imagen.
+    Retorna el modelo best.pt que se utilizará en producción.
+    '''
     modelo = YOLO(MODELO)
 
     resultados = modelo.train(data=str(RUTA_DATOS), epochs=EPOCHS, imgsz=TAMANIO_IMAGEN, project=PROYECTO,name=NOMBRE_MODELO)
